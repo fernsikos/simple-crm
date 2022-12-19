489aef5e9993ea9c8de31fe0b40ca7a058211398
Tobias Fernkorn <fernsikos@gmail.com>
2022-12-19 15:32:54 +0700

    variables binded

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/models/user.class';
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dialog-add-user.component.scss']
 })
 export class DialogAddUserComponent implements OnInit {
+
+  user = new User();
 
   constructor() { }
 
